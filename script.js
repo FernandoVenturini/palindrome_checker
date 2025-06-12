0faafb3b32bb249efd3138fb1b328d40f31744d7
Fernando <fernandoventurini9@hotmail.com>
2025-06-12 13:51:46 +0100

    palindrome checker

---
+++ script.js
@@ -0,0 +1,15 @@
+const wordInput = document.querySelector("#word");
+const btnCheck = document.querySelector("#btnCheck");
+const result = document.querySelector("#result");
+
+function checkPalindrome() {
+    const word = wordInput.value;
+    const invertedWord = word.split("").reverse().join("");
+    if (word.toLowerCase() === invertedWord.toLowerCase()) {
+        result.textContent = `The word "${word}" is palindrome!`;
+    } else {
+        result.textContent = `The word "${word}" is not a palindrome!`;
+    };
+}
+
+btnCheck.addEventListener("click", checkPalindrome);
